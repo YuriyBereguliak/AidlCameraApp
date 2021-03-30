2efd2ffc6167e2d9ffbbb69e1db836458796a3ed
Yuriy Bereguliak <ybereguliak95@gmail.com>
2021-03-30 20:40:45 +0300

    ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS

--- camera_info/src/main/cpp/native-lib.cpp
+++ camera_info/src/main/cpp/native-lib.cpp
@@ -17,6 +17,7 @@
 #define TAG "camera_info"
 #define LOGD(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 
+//https://developer.android.com/ndk/reference/group/camera#group___camera_1gga49cf3e5a3deefe079ad036a8fac14627ab4ef4fabbbaaecf6f2fc74eaa9197b26
 static void initCam() {
     ACameraManager *cameraManager = ACameraManager_create();
     ACameraIdList *cameraIds = nullptr;
@@ -33,7 +34,34 @@ static void initCam() {
     for (int i = 0; i < cameraIds->numCameras; ++i) {
         const char *id = cameraIds->cameraIds[i];
         LOGD("camera id %s", id);
-//        printCamProps(cameraManager, id);
+
+        ACameraMetadata *metadataObj;
+        ACameraManager_getCameraCharacteristics(cameraManager, id, &metadataObj);
+
+        ACameraMetadata_const_entry entry = {0};
+
+        /// ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS
+        /// The available stream configurations that this camera device supports
+        /// The configurations are listed as (format, width, height, input?) tuples.
+        ACameraMetadata_getConstEntry(metadataObj,
+                                      ACAMERA_SCALER_AVAILABLE_STREAM_CONFIGURATIONS,
+                                      &entry);
+        for (int y = 0; y < entry.count; y += 4) {
+            int32_t input = entry.data.i32[y + 3];
+            if (input) continue;
+
+            int32_t format = entry.data.i32[y + 0];
+            /// enum
+            /// This format is always supported as an output format for the android Camera2 NDK API.
+            if (format == AIMAGE_FORMAT_JPEG) {
+                int32_t width = entry.data.i32[y + 1];
+                int32_t height = entry.data.i32[y + 2];
+
+                LOGD("camProps: width=%d vs height=%d", width, height);
+            }
+        }
+
+        *metadataObj = nullptr;
     }
 
     ACameraManager_deleteCameraIdList(cameraIds);
